d9e50168df040260b7c356ccec605ca8a920a7c9
Codigo Estudiante <jchambi97@hotmail.com>
2024-03-28 16:57:28 -0500

    first commit

---
+++ Registro/Registro/Form1.Designer.vb
@@ -0,0 +1,324 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtnombre = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtcodigo = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txttelefono = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtapellido = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.dgregistros = New System.Windows.Forms.DataGridView()
+        Me.gbdatos = New System.Windows.Forms.GroupBox()
+        Me.btnbuscar = New System.Windows.Forms.Button()
+        Me.gbregistros = New System.Windows.Forms.GroupBox()
+        Me.btneliminar = New System.Windows.Forms.Button()
+        Me.btncancelar = New System.Windows.Forms.Button()
+        Me.btnaceptar = New System.Windows.Forms.Button()
+        Me.btnagregar = New System.Windows.Forms.Button()
+        Me.btnmodificar = New System.Windows.Forms.Button()
+        Me.btnnuevo = New System.Windows.Forms.Button()
+        Me.picfoto = New System.Windows.Forms.PictureBox()
+        Me.lbltexto = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        CType(Me.dgregistros, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.gbdatos.SuspendLayout()
+        Me.gbregistros.SuspendLayout()
+        CType(Me.picfoto, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(140, 22)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(52, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Apellidos:"
+        '
+        'txtnombre
+        '
+        Me.txtnombre.Location = New System.Drawing.Point(58, 55)
+        Me.txtnombre.Name = "txtnombre"
+        Me.txtnombre.Size = New System.Drawing.Size(134, 20)
+        Me.txtnombre.TabIndex = 2
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(6, 58)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(47, 13)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "Nombre:"
+        '
+        'txtcodigo
+        '
+        Me.txtcodigo.Location = New System.Drawing.Point(58, 19)
+        Me.txtcodigo.Name = "txtcodigo"
+        Me.txtcodigo.Size = New System.Drawing.Size(64, 20)
+        Me.txtcodigo.TabIndex = 1
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(10, 22)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(43, 13)
+        Me.Label3.TabIndex = 0
+        Me.Label3.Text = "Codigo:"
+        '
+        'txttelefono
+        '
+        Me.txttelefono.Location = New System.Drawing.Point(256, 55)
+        Me.txttelefono.Name = "txttelefono"
+        Me.txttelefono.Size = New System.Drawing.Size(85, 20)
+        Me.txttelefono.TabIndex = 4
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(198, 58)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(52, 13)
+        Me.Label4.TabIndex = 0
+        Me.Label4.Text = "Telefono:"
+        '
+        'txtapellido
+        '
+        Me.txtapellido.Location = New System.Drawing.Point(197, 19)
+        Me.txtapellido.Name = "txtapellido"
+        Me.txtapellido.Size = New System.Drawing.Size(133, 20)
+        Me.txtapellido.TabIndex = 3
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Arial Narrow", 26.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(63, 34)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(294, 42)
+        Me.Label5.TabIndex = 3
+        Me.Label5.Text = "Registro de Personas"
+        '
+        'dgregistros
+        '
+        Me.dgregistros.BackgroundColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.dgregistros.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgregistros.Location = New System.Drawing.Point(15, 19)
+        Me.dgregistros.Name = "dgregistros"
+        Me.dgregistros.ReadOnly = True
+        Me.dgregistros.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
+        Me.dgregistros.Size = New System.Drawing.Size(501, 229)
+        Me.dgregistros.TabIndex = 4
+        '
+        'gbdatos
+        '
+        Me.gbdatos.Controls.Add(Me.txtapellido)
+        Me.gbdatos.Controls.Add(Me.Label4)
+        Me.gbdatos.Controls.Add(Me.txtcodigo)
+        Me.gbdatos.Controls.Add(Me.txttelefono)
+        Me.gbdatos.Controls.Add(Me.Label2)
+        Me.gbdatos.Controls.Add(Me.Label3)
+        Me.gbdatos.Controls.Add(Me.txtnombre)
+        Me.gbdatos.Controls.Add(Me.Label1)
+        Me.gbdatos.Location = New System.Drawing.Point(27, 78)
+        Me.gbdatos.Name = "gbdatos"
+        Me.gbdatos.Size = New System.Drawing.Size(355, 88)
+        Me.gbdatos.TabIndex = 6
+        Me.gbdatos.TabStop = False
+        '
+        'btnbuscar
+        '
+        Me.btnbuscar.Location = New System.Drawing.Point(473, 172)
+        Me.btnbuscar.Name = "btnbuscar"
+        Me.btnbuscar.Size = New System.Drawing.Size(45, 23)
+        Me.btnbuscar.TabIndex = 7
+        Me.btnbuscar.Text = "..."
+        Me.btnbuscar.UseVisualStyleBackColor = True
+        '
+        'gbregistros
+        '
+        Me.gbregistros.Controls.Add(Me.dgregistros)
+        Me.gbregistros.Location = New System.Drawing.Point(12, 225)
+        Me.gbregistros.Name = "gbregistros"
+        Me.gbregistros.Size = New System.Drawing.Size(533, 266)
+        Me.gbregistros.TabIndex = 9
+        Me.gbregistros.TabStop = False
+        Me.gbregistros.Text = "Registros Totales "
+        '
+        'btneliminar
+        '
+        Me.btneliminar.Image = Global.Registro.My.Resources.Resources.delete
+        Me.btneliminar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.btneliminar.Location = New System.Drawing.Point(214, 172)
+        Me.btneliminar.Name = "btneliminar"
+        Me.btneliminar.Size = New System.Drawing.Size(83, 35)
+        Me.btneliminar.TabIndex = 5
+        Me.btneliminar.Text = "Eliminar"
+        Me.btneliminar.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.btneliminar.UseVisualStyleBackColor = True
+        '
+        'btncancelar
+        '
+        Me.btncancelar.Image = Global.Registro.My.Resources.Resources.dialog_cancel
+        Me.btncancelar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.btncancelar.Location = New System.Drawing.Point(214, 172)
+        Me.btncancelar.Name = "btncancelar"
+        Me.btncancelar.Size = New System.Drawing.Size(83, 35)
+        Me.btncancelar.TabIndex = 5
+        Me.btncancelar.Text = "Cancelar"
+        Me.btncancelar.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.btncancelar.UseVisualStyleBackColor = True
+        '
+        'btnaceptar
+        '
+        Me.btnaceptar.Image = Global.Registro.My.Resources.Resources.check_file_24
+        Me.btnaceptar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.btnaceptar.Location = New System.Drawing.Point(125, 172)
+        Me.btnaceptar.Name = "btnaceptar"
+        Me.btnaceptar.Size = New System.Drawing.Size(83, 35)
+        Me.btnaceptar.TabIndex = 5
+        Me.btnaceptar.Text = "Aceptar"
+        Me.btnaceptar.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.btnaceptar.UseVisualStyleBackColor = True
+        '
+        'btnagregar
+        '
+        Me.btnagregar.Image = Global.Registro.My.Resources.Resources.save
+        Me.btnagregar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.btnagregar.Location = New System.Drawing.Point(36, 172)
+        Me.btnagregar.Name = "btnagregar"
+        Me.btnagregar.Size = New System.Drawing.Size(83, 35)
+        Me.btnagregar.TabIndex = 5
+        Me.btnagregar.Text = "Agregar"
+        Me.btnagregar.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.btnagregar.UseVisualStyleBackColor = True
+        '
+        'btnmodificar
+        '
+        Me.btnmodificar.Image = Global.Registro.My.Resources.Resources.edit
+        Me.btnmodificar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.btnmodificar.Location = New System.Drawing.Point(125, 172)
+        Me.btnmodificar.Name = "btnmodificar"
+        Me.btnmodificar.Size = New System.Drawing.Size(83, 35)
+        Me.btnmodificar.TabIndex = 5
+        Me.btnmodificar.Text = "Modificar"
+        Me.btnmodificar.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.btnmodificar.UseVisualStyleBackColor = True
+        '
+        'btnnuevo
+        '
+        Me.btnnuevo.Image = Global.Registro.My.Resources.Resources.icono_clientes
+        Me.btnnuevo.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.btnnuevo.Location = New System.Drawing.Point(36, 172)
+        Me.btnnuevo.Name = "btnnuevo"
+        Me.btnnuevo.Size = New System.Drawing.Size(83, 35)
+        Me.btnnuevo.TabIndex = 5
+        Me.btnnuevo.Text = "Nuevo"
+        Me.btnnuevo.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.btnnuevo.UseVisualStyleBackColor = True
+        '
+        'picfoto
+        '
+        Me.picfoto.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.picfoto.Location = New System.Drawing.Point(401, 34)
+        Me.picfoto.Name = "picfoto"
+        Me.picfoto.Size = New System.Drawing.Size(117, 132)
+        Me.picfoto.TabIndex = 2
+        Me.picfoto.TabStop = False
+        '
+        'lbltexto
+        '
+        Me.lbltexto.AutoSize = True
+        Me.lbltexto.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbltexto.Location = New System.Drawing.Point(13, 9)
+        Me.lbltexto.Name = "lbltexto"
+        Me.lbltexto.Size = New System.Drawing.Size(36, 16)
+        Me.lbltexto.TabIndex = 10
+        Me.lbltexto.Text = "texto"
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 60
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Gainsboro
+        Me.ClientSize = New System.Drawing.Size(556, 508)
+        Me.Controls.Add(Me.lbltexto)
+        Me.Controls.Add(Me.gbregistros)
+        Me.Controls.Add(Me.btnbuscar)
+        Me.Controls.Add(Me.gbdatos)
+        Me.Controls.Add(Me.btneliminar)
+        Me.Controls.Add(Me.btncancelar)
+        Me.Controls.Add(Me.btnaceptar)
+        Me.Controls.Add(Me.btnagregar)
+        Me.Controls.Add(Me.btnmodificar)
+        Me.Controls.Add(Me.btnnuevo)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.picfoto)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Proyecto 01 by Louix Chambi"
+        CType(Me.dgregistros, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.gbdatos.ResumeLayout(False)
+        Me.gbdatos.PerformLayout()
+        Me.gbregistros.ResumeLayout(False)
+        CType(Me.picfoto, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents txtnombre As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents txtcodigo As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents txttelefono As System.Windows.Forms.TextBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents txtapellido As System.Windows.Forms.TextBox
+    Friend WithEvents picfoto As System.Windows.Forms.PictureBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents dgregistros As System.Windows.Forms.DataGridView
+    Friend WithEvents btnnuevo As System.Windows.Forms.Button
+    Friend WithEvents btnmodificar As System.Windows.Forms.Button
+    Friend WithEvents btncancelar As System.Windows.Forms.Button
+    Friend WithEvents btnagregar As System.Windows.Forms.Button
+    Friend WithEvents btnaceptar As System.Windows.Forms.Button
+    Friend WithEvents btneliminar As System.Windows.Forms.Button
+    Friend WithEvents gbdatos As System.Windows.Forms.GroupBox
+    Friend WithEvents btnbuscar As System.Windows.Forms.Button
+    Friend WithEvents gbregistros As System.Windows.Forms.GroupBox
+    Friend WithEvents lbltexto As System.Windows.Forms.Label
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+
+End Class
---
+++ Registro/Registro/Form1.vb
@@ -0,0 +1,258 @@
+﻿Imports System.Data.OleDb
+
+Public Class Form1
+    Public con As New OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\Users\casa\Desktop\Base de datos\Registro.mdb")
+    Public cmd As New OleDbCommand
+    Public sql As String
+    Public texto As String = "Escribe en los comentarios, que otros proyectos deseas y dale LIKE si te gusto el vídeo   "
+    Dim textosub As String = ""
+    Dim c As Integer
+
+
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Try
+            con.Open()
+            MessageBox.Show("Se conectó correctamente con la base de datos")
+            ControlesIniciales()
+            MostrarRegistros()
+            Timer1.Start()
+        Catch ex As Exception
+            MessageBox.Show("Base de datos no encontrada " + ex.Message)
+        End Try
+    End Sub
+    Public Sub limpiarcajas()
+        For Each caja In gbdatos.Controls
+            If TypeOf (caja) Is TextBox Then
+                caja.Text = ""
+                caja.Enabled = True
+            End If
+        Next
+    End Sub
+    Public Sub ControlesIniciales()
+        btnnuevo.Visible = True
+        btnagregar.Visible = False
+
+        btnmodificar.Visible = True
+        btnaceptar.Visible = False
+
+        btneliminar.Visible = True
+        btncancelar.Visible = False
+
+        btnbuscar.Visible = False
+        picfoto.Image = Nothing
+        limpiarcajas()
+        gbdatos.Enabled = False
+    End Sub
+    Public Sub MostrarRegistros()
+        sql = ""
+        sql = "SELECT id_persona, nombre, apellido, telefono, foto FROM personas"
+        cmd = New OleDbCommand(sql, con)
+        Dim adp As New OleDbDataAdapter(cmd)
+        Dim dt As New DataTable
+        adp.Fill(dt)
+        dgregistros.DataSource = dt
+
+        Dim c As Integer
+        c = dt.Rows.Count
+        gbregistros.Text = "Registros Totales " + c.ToString()
+        dgregistros.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill 'rellena todo el data gridview
+        dgregistros.Columns.Item(4).Visible = False
+        dgregistros.AllowUserToAddRows = False
+    End Sub
+
+    Private Sub btnnuevo_Click(sender As Object, e As EventArgs) Handles btnnuevo.Click
+        limpiarcajas()
+        btnnuevo.Visible = False
+        btnagregar.Visible = True
+
+        btnmodificar.Visible = False
+        btnaceptar.Visible = False
+
+        btncancelar.Visible = True
+        btneliminar.Visible = False
+
+        btnbuscar.Visible = True
+        picfoto.Image = Nothing
+        gbdatos.Enabled = True
+    End Sub
+
+    Private Sub btncancelar_Click(sender As Object, e As EventArgs) Handles btncancelar.Click
+        ControlesIniciales()
+    End Sub
+
+    Private Sub btnmodificar_Click(sender As Object, e As EventArgs) Handles btnmodificar.Click
+        Dim condicion As Boolean = txtcodigo.Text <> "" And txtnombre.Text <> "" And txtapellido.Text <> "" And txttelefono.Text <> ""
+
+        If condicion Then
+            txtcodigo.Enabled = False
+            btnmodificar.Visible = False
+            btnaceptar.Visible = True
+
+            btnnuevo.Visible = False
+            btnagregar.Visible = False
+
+            btncancelar.Visible = True
+            btneliminar.Visible = False
+
+            btnbuscar.Visible = True
+
+            gbdatos.Enabled = True
+        Else
+            MessageBox.Show("Seleccione un registro", "Registro informa", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        End If
+
+
+    End Sub
+    Private Sub btneliminar_Click(sender As Object, e As EventArgs) Handles btneliminar.Click
+        Dim condicion As Boolean = txtcodigo.Text <> "" And txtnombre.Text <> "" And txtapellido.Text <> "" And txttelefono.Text <> ""
+
+
+        If condicion Then
+            Dim pregunta As DialogResult = MessageBox.Show("Desea eliminar el registro seleccionado", "Registro Informa", MessageBoxButtons.OKCancel, MessageBoxIcon.Question)
+            If pregunta = Windows.Forms.DialogResult.OK Then
+                sql = ""
+                sql = "DELETE FROM personas WHERE id_persona = @cod"
+                cmd = New OleDbCommand(sql, con)
+                cmd.Parameters.Add(New OleDbParameter("@cod", txtcodigo.Text))
+                If cmd.ExecuteNonQuery Then
+                    MessageBox.Show("Registro elminado")
+                    MostrarRegistros()
+                    ControlesIniciales()
+                Else
+                    MessageBox.Show("Registro no eliminado")
+                End If
+            End If
+           
+        Else
+            MessageBox.Show("Seleccione un registro", "Registro informa", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        End If
+
+    End Sub
+
+    Private Sub btnagregar_Click(sender As Object, e As EventArgs) Handles btnagregar.Click
+        Dim condicion As Boolean = txtcodigo.Text <> "" And txtnombre.Text <> "" And txtapellido.Text <> "" And txttelefono.Text <> ""
+        If condicion Then
+            Try
+                sql = ""
+                sql = "INSERT INTO personas VALUES(@cod,@nom,@Ape,@tel,@fot)"
+                cmd = New OleDbCommand(sql, con)
+                cmd.Parameters.Add(New OleDbParameter("@cod", txtcodigo.Text))
+                cmd.Parameters.Add(New OleDbParameter("@nom", txtnombre.Text))
+                cmd.Parameters.Add(New OleDbParameter("@ape", txtapellido.Text))
+                cmd.Parameters.Add(New OleDbParameter("@tel", txttelefono.Text))
+
+                Dim i() As Byte
+                Dim ms As New IO.MemoryStream()
+
+                If picfoto.Image Is Nothing Then
+                    picfoto.Image = My.Resources.Sin_imagen_disponible
+                End If
+                picfoto.SizeMode = PictureBoxSizeMode.StretchImage
+                picfoto.Image.Save(ms, picfoto.Image.RawFormat)
+                i = ms.GetBuffer
+
+                cmd.Parameters.Add(New OleDbParameter("@fot", i))
+
+                If cmd.ExecuteNonQuery Then
+                    MessageBox.Show("Registro correcto", "Registro Informa", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                    ControlesIniciales()
+                    MostrarRegistros()
+                Else
+                    MessageBox.Show("Registro no correcto", "Registro Informa", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+                    ControlesIniciales()
+                    MostrarRegistros()
+                End If
+            Catch ex As Exception
+                MessageBox.Show(ex.Message)
+            End Try
+        Else
+            MessageBox.Show("Rellene todos los Datos", "Registro Informa", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        End If
+        
+    End Sub
+
+    Private Sub btnaceptar_Click(sender As Object, e As EventArgs) Handles btnaceptar.Click
+        Dim condicion As Boolean = txtcodigo.Text <> "" And txtnombre.Text <> "" And txtapellido.Text <> "" And txttelefono.Text <> ""
+        If condicion Then
+            Try
+                sql = ""
+                sql = "UPDATE personas SET id_persona = @cod, nombre = @nom, apellido = @ape, telefono = @tel, foto = @fot WHERE id_persona = @cod "
+                cmd = New OleDbCommand(sql, con)
+                cmd.Parameters.Add(New OleDbParameter("@cod", txtcodigo.Text))
+                cmd.Parameters.Add(New OleDbParameter("@nom", txtnombre.Text))
+                cmd.Parameters.Add(New OleDbParameter("@ape", txtapellido.Text))
+                cmd.Parameters.Add(New OleDbParameter("@tel", txttelefono.Text))
+
+                Dim i() As Byte
+                Dim ms As New IO.MemoryStream()
+
+                If picfoto.Image Is Nothing Then
+                    picfoto.Image = My.Resources.Sin_imagen_disponible
+                End If
+                picfoto.SizeMode = PictureBoxSizeMode.StretchImage
+                picfoto.Image.Save(ms, picfoto.Image.RawFormat)
+                i = ms.GetBuffer
+
+                cmd.Parameters.Add(New OleDbParameter("@fot", i))
+
+                If cmd.ExecuteNonQuery Then
+                    MessageBox.Show("Modificado correcto", "Registro Informa", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                    ControlesIniciales()
+                    MostrarRegistros()
+                Else
+                    MessageBox.Show("Modificado no correcto", "Registro Informa", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+                    ControlesIniciales()
+                    MostrarRegistros()
+                End If
+            
+
+            Catch ex As Exception
+                MessageBox.Show(ex.Message)
+            End Try
+        Else
+            MessageBox.Show("Rellene todos los Datos", "Registro Informa", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        End If
+    End Sub
+
+    Private Sub dgregistros_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles dgregistros.CellClick
+        txtcodigo.Text = dgregistros.SelectedCells.Item(0).Value
+        txtnombre.Text = dgregistros.SelectedCells.Item(1).Value
+        txtapellido.Text = dgregistros.SelectedCells.Item(2).Value
+        txttelefono.Text = dgregistros.SelectedCells.Item(3).Value
+
+        picfoto.Image = Nothing
+
+        Dim i() As Byte = dgregistros.SelectedCells.Item(4).Value
+        Dim ms As New IO.MemoryStream(i)
+
+        picfoto.Image = Image.FromStream(ms)
+        picfoto.SizeMode = PictureBoxSizeMode.StretchImage
+    End Sub
+
+    Private Sub btnbuscar_Click(sender As Object, e As EventArgs) Handles btnbuscar.Click
+        Dim openfiledialog As New OpenFileDialog
+
+        openfiledialog.Filter = "Imagen jpg|*.jpg"
+        If openfiledialog.ShowDialog = Windows.Forms.DialogResult.OK Then
+            'picfoto.Image = Image.FromFile(openfiledialog.FileName)
+            picfoto.Image = New Bitmap(openfiledialog.FileName)
+            picfoto.SizeMode = PictureBoxSizeMode.StretchImage
+        End If
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+       
+        Dim largo As Integer = texto.Length - 1
+        textosub += texto.Substring(c, 1)
+        c += 1
+        lbltexto.Text = textosub
+
+        If largo = c Then
+            c = 0
+            textosub = ""
+        End If
+    End Sub
+End Class
